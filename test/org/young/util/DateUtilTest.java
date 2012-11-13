a3bd53855dafdbd0c5a4322ea507b492462d6a76
PC-Young <Administrator@PC-YOUNG>
2012-11-13 09:00:54 +0800

    Test for addDay

--- test/org/young/util/DateUtilTest.java
+++ test/org/young/util/DateUtilTest.java
@@ -15,4 +15,17 @@ public class DateUtilTest {
 		System.out.println(newDate);
 	}
 	
+	@Test
+	public void testAddDay() {
+		// Test Now
+		Date date1 = new Date();
+		Date date2 = DateUtil.addDay(1, date1);
+		
+		System.out.println(date2);
+		
+		// Test
+		Date date3 = DateUtil.getFormatDate(null, "2012-11-12 22:00:00");
+		Date date4 = DateUtil.addDay(99, date3);
+		System.out.println(date4);
+	}
 }
