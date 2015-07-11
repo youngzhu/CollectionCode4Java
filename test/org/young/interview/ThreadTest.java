c45870893072c488744da42686f6c254ef380fea
ubuntu-work <young@ubuntu13.com>
2015-07-11 21:41:33 +0800

    The difference between Java 8 and Java 7

--- test/org/young/interview/ThreadTest.java
+++ test/org/young/interview/ThreadTest.java
@@ -3,16 +3,71 @@ package org.young.interview;
 import org.junit.Test;
 
 public class ThreadTest {
-
+	
+	private String str1 = "ZZZZ";
+	
 	@Test
 	public void testFinalVar() {
-	
-		String str = "test";
 		
+//		final String str = "test";
+		/*
+		 * For Java 8, it's OK .
+		 * But for Java 7 or earlier, it must be declared as final
+		 */
+		String str = "test";
+	
 		(new Runnable() {
-			public void run() {System.out.println(str);};
+			/*
+			 * 
+			 *  Cannot refer to a non-final variable str inside an inner class defined in a different method
+			 */
+			public void run() {
+				System.out.println(str);
+				System.out.println(str1);
+			}
 		}).run();
 	}
 	
+	@Test
+	public void testA() {
+		A a = new A();
+		System.out.println(a.getStr());
+	}
+	
+	@Test
+	public void testB() {
+		new B().setB(str1);
+		
+		String s = "BBBB";
+		new B().setB(s);
+	}
+	
+	class A {
+		private String a;
+		
+		public A() {
+			a = str1;
+		}
+		
+		public String getStr() {
+			return a;
+		}
+		
+		public void setStr(String a) {
+			this.a = a;
+		}
+	}
+	
+	class B {
+		private String b;
 
+		public String getB() {
+			return b;
+		}
+
+		public void setB(String b) {
+			this.b = b;
+		}
+		
+	}
 }
