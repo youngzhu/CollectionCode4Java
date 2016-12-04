b522b9854c46b62c26dcb44ad45c03c030755379
young-win8 <young.zhu.2012@gmail.com>
2016-12-04 16:22:36 +0800

    THE STRATEGY PATTERN

---
+++ src/org/young/elearn/patterns/behavioral/strategy/IndexProgrammingLanguagesWithNormal.java
@@ -0,0 +1,12 @@
+package org.young.elearn.patterns.behavioral.strategy;
+
+import java.util.List;
+
+public class IndexProgrammingLanguagesWithNormal {
+	
+	private static final List<String> LANGUAGES = {};
+
+	public static void main(String[] args) {
+		
+	}
+}
