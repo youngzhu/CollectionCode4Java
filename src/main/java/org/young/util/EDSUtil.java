d81b076a8b8d663a300837a327671e0c1758f3fd
Young-Mac <young.zhu.2012@gmail.com>
2020-04-28 23:57:59 +0800

    eds week

--- src/main/java/org/young/util/EDSUtil.java
+++ src/main/java/org/young/util/EDSUtil.java
@@ -148,12 +148,14 @@ public class EDSUtil extends HttpRequestUtil {
         BufferedReader in = null;
         String result = "";
         
-        String param = "__EVENTTARGET=submitButton&__EVENTARGUMENT=&__VIEWSTATE=%2FwEPDwUKMjE0NTI1MTkwOA8WAh4MRGVwYXJ0bWVudElEBQIzN2RkOfRVTbw%2FrUknLeGOXU44DaBFoZU%3D&__VIEWSTATEGENERATOR=F286991A&__EVENTVALIDATION=%2FwEWDAL9wPaHDQLjqP%2F%2BDQLd8ZHcCQLhovT5BALDnO%2FYDAL7u6OWDQL45bFcArmMv4wMAuqi3IQOAvKI0Y0HAoL54voCAouPw9wIQ97BtMk7wNFID0s1jIASxAkOZ6Q%3D&WeekReportDate=";
+        String param = "__VIEWSTATE=%2FwEPDwUJMzIwMzY2NTgyD2QWAgIBD2QWCAIjDxYCHgdWaXNpYmxlaGQCJQ8WAh8AaBYCZg9kFgJmD2QWAgIDDxBkZBYAZAInDxYCHwBoZAIpDxYCHwBoZGTPTMc1nYRqbEKCiICYhOSgnUGv%2Fg%3D%3D&__VIEWSTATEGENERATOR=9F626D6E&__EVENTVALIDATION=%2FwEWCgKI1pXCBgLenYyRDwK4mtSdBQLjqP%2F%2BDQKVzZi4AgLIuY3nBALs5I6BBgKyyYHDDgL8oMWmCQLCi9reA9s3WTxWqzkDTYIVWZG%2BfybX6nyl&hidCurrRole=&hidWeeklyState=&WeekReportDate=";
         param += logDate;
-        param += "&Textplan=PPAI-943++%E4%BA%BA%E6%84%8F%E9%99%A9%E8%A1%A8%E9%87%8D%E6%9E%84%E4%BC%98%E5%8C%96%EF%BC%9A%E7%AC%AC%E4%BA%8C%E6%89%B9%E6%94%B9%E9%80%A0%EF%BC%9A%E4%BB%8Etbl_plclink%E8%8E%B7%E5%8F%96%E4%BF%9D%E5%8D%95%E5%8F%B7%E7%9A%84%E6%94%B9%E9%80%A0%E4%B8%BA%E7%9B%B4%E6%8E%A5%E4%BB%8E%E4%B8%BB%E8%A1%A8tbl_plcinfo%E8%8E%B7%E5%8F%96%EF%BC%88REQPDS-909%EF%BC%89+&Textunfinish=done&Textproblem=%E7%9F%A5%E5%85%B6%E7%84%B6%EF%BC%8C%E4%B8%8D%E7%9F%A5%E5%85%B6%E6%89%80%E4%BB%A5%E7%84%B6&Textsolution=Just+have+a+try%21%21&Textdefect=%E6%96%87%E5%AD%97%E8%A1%A8%E8%BE%BE%0D%0A%0D%0A%E4%BB%A3%E7%A0%81%E8%80%83%E8%99%91%E4%B8%8D%E5%91%A8%E5%85%A8&Textnextweek=+PPAI-980++%E8%AF%B7%E5%B0%86%E4%BA%BA%E6%84%8F%E9%99%A9%E7%B3%BB%E7%BB%9F%E7%B3%BB%E7%BB%9F%E4%B8%AD%E7%BB%9F%E4%BF%9D%E4%B8%9A%E5%8A%A1%E4%BB%A3%E7%A0%81%E4%B8%BADL006%E7%9A%84%E4%B8%9A%E5%8A%A1%E6%9B%B4%E6%94%B9%E4%B8%BA%22%E8%A7%81%E8%B4%B9%E5%87%BA%E5%8D%95%22%E4%B8%9A%E5%8A%A1%E3%80%82%EF%BC%88RPDAL-145%EF%BC%89+%0D%0A+%0D%0A+%0D%0A++%0D%0A%0D%0A%0D%0A+%0D%0A+&Textsuggestion=%E6%97%A0";
-        
+        param += "&txtWorkContent=code%2Btest&txtStudyContent=refactor&txtSummary=no+pain+no+gain&txtPlanWork=wechat+message&txtPlanStudy=algorithm+&btnSubmit=%E6%8F%90%E4%BA%A4";
+
+
         try {
-            URL realUrl = new URL("http://eds.newtouch.cn/eds36web/files/report/weekreport_add.aspx?HasReturn=1" + logDate);
+            final String url = "http://eds.newtouch.cn/eds36web/WorkWeekly/WorkWeeklyInfo.aspx";
+            URL realUrl = new URL(url);
             // 打开和URL之间的连接
             URLConnection conn = realUrl.openConnection();
             // 设置通用的请求属性
@@ -166,7 +168,7 @@ public class EDSUtil extends HttpRequestUtil {
 //            Origin: http://eds.newtouch.cn
             conn.setRequestProperty("Origin", "http://eds.newtouch.cn");
 //            Upgrade-Insecure-Requests: 1
-            conn.setRequestProperty("Upgrade-Insecure-Requests", "1");
+//            conn.setRequestProperty("Upgrade-Insecure-Requests", "1");
 //            Content-Type: application/x-www-form-urlencoded
             conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 //            User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36
@@ -174,13 +176,13 @@ public class EDSUtil extends HttpRequestUtil {
 //            Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
             conn.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
 //            Referer: http://eds.newtouch.cn/eds3/worklog.aspx?LogDate=2018-06-05
-            conn.setRequestProperty("Referer", "http://eds.newtouch.cn/eds36web/files/report/weekreport_add.aspx?HasReturn=1");
+            conn.setRequestProperty("Referer", url);
 //            Accept-Encoding: gzip, deflate
             conn.setRequestProperty("Accept-Encoding", ACCEPT_ENCODING);
 //            Accept-Language: zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7
             conn.setRequestProperty("Accept-Language", ACCEPT_LANGUAGE);
 //            Cookie: ASP.NET_SessionId=e32ox145snsbgn45r3yfnmnl; ActionSelect=; PersonId=12234
-            conn.setRequestProperty("Cookie", "ASP.NET_SessionId=e32ox145snsbgn45r3yfnmnl; ActionSelect=; PersonId=12234");
+            conn.setRequestProperty("Cookie", "ASP.NET_SessionId=4khtnz55xiyhbmncrzmzyzzc; ActionSelect=010601; Hm_lvt_416c770ac83a9d996d7b3793f8c4994d=1569767826; Hm_lpvt_416c770ac83a9d996d7b3793f8c4994d=1569767826; Hm_lvt_41b5c597ee994508516d3536526ea9d3=1580656307; Hm_lpvt_41b5c597ee994508516d3536526ea9d3=1580656307; PersonId=12234");
             
 //            conn.setRequestProperty("connection", "Keep-Alive");
             
--- src/test/java/org/young/util/EDSUtilTest.java
+++ src/test/java/org/young/util/EDSUtilTest.java
@@ -6,10 +6,12 @@ import org.junit.Test;
 import java.util.Date;
 
 public class EDSUtilTest extends EDSUtil {
+
+	final String date = "2020-04-27";
 	
 	@Test
 	public void testLogToEDS() throws Exception {
-		Date from = DateUtil.getFormatDate(DateUtil.DATE_PATTERN_YMD, "2020-04-06");
+		Date from = DateUtil.getFormatDate(DateUtil.DATE_PATTERN_YMD, date);
 		String dateStr;
 		
 		for (int i = 0; i < 5; i++) {
@@ -25,19 +27,7 @@ public class EDSUtilTest extends EDSUtil {
 	
 	@Test
 	public void testWeekReportToEDS() throws Exception {
-		Date from = DateUtil.getFormatDate(DateUtil.DATE_PATTERN_YMD, "2018-07-16");
-		Date to = DateUtil.getFormatDate(DateUtil.DATE_PATTERN_YMD, "2019-01-11");
-		
-		String dateStr;
-		do {
-			dateStr = DateUtil.getFormatDate(DateUtil.DATE_PATTERN_YMD, from);
-			System.out.println(dateStr);
-			weekReportToEDS(dateStr);
-			long interval = (long)(Math.random() * 4000L);
-			System.out.println(interval);
-			Thread.sleep(interval);
-			from = DateUtil.addDay(7, from);
-		} while (from.before(to));
+		weekReportToEDS(date);
 	}
 
 }
